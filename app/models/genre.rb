e3549a0f22ee79e30757025b1273f2b6c4303e1e
Logan Smith <logsmith06@gmail.com>
2020-02-03 19:51:34 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.songs.each do |name|
+    self.each do |name|
       name.name
     end
 
